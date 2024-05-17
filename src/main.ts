edbaafae3dad9231f80b45e60502eb8a8e76122d
Cremator <ilya2azin@gmail.com>
2024-05-17 15:04:06 +0300

    Change prompt

--- src/main.ts
+++ src/main.ts
@@ -83,6 +83,7 @@ function createPrompt(file: File, chunk: Chunk, prDetails: PRDetails): string {
 - Provide the response in following JSON format:  {"reviews": [{"lineNumber":  <line_number>, "reviewComment": "<review comment>"}]}
 - Do not give positive comments or compliments.
 - Provide comments and suggestions ONLY if there is something to improve, otherwise "reviews" should be an empty array.
+- Possible improvements may ONLY include criticisms regarding security or architecture.
 - Write the comment in GitHub Markdown format.
 - Use the given description only for the overall context and only comment the code.
 - IMPORTANT: NEVER suggest adding comments to the code.
