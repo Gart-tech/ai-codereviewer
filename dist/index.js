53a73e0c42e8b8bb0b232bec4830cc7a15bcd568
shkrobik2017 <shkrobik2017@gmail.com>
2024-07-24 00:20:12 +0300

    update-index.js

--- dist/index.js
+++ dist/index.js
@@ -106,16 +106,25 @@ function analyzeCode(parsedDiff, prDetails) {
     });
 }
 function createPrompt(file, chunk, prDetails) {
-    return `Your task is to review pull requests. Instructions:
+    const botName = core.getInput("bot_name");
+    const rules = core.getInput("rules");
+    const rulesPrompt = rules === ""
+        ? ""
+        : `Your review will *only* ensure the following rules are followed:
+${rules}`;
+    return `Your name is ${botName}. Your task is to review pull requests. ${rulesPrompt}
+
+Here are your instructions regarding the format and the style of the review:
 - Provide the response in following JSON format:  {"reviews": [{"lineNumber":  <line_number>, "reviewComment": "<review comment>"}]}
 - Do not give positive comments or compliments.
 - Provide comments and suggestions ONLY if there is something to improve, otherwise "reviews" should be an empty array.
 - Write the comment in GitHub Markdown format.
+- You can suggest a fix in a reviewComment if you want to by using a \`\`\`suggestion\`\`\` code block (with proper whitespace indentation).
 - Use the given description only for the overall context and only comment the code.
 - IMPORTANT: NEVER suggest adding comments to the code.
 
 Review the following code diff in the file "${file.to}" and take the pull request title and description into account when writing the response.
-  
+
 Pull request title: ${prDetails.title}
 Pull request description:
 
@@ -134,28 +143,47 @@ ${chunk.changes
 \`\`\`
 `;
 }
+/**
+ * Checks if the given model supports JSON object mode.
+ *
+ * The function determines if a model supports JSON object mode by checking
+ * if the model's name includes any of the prefixes specified in the
+ * `supportedJsonObjectModelsPrefix` array. The supported models include
+ * variants of GPT-4-turbo, GPT-3.5-turbo, and GPT-4o.
+ *
+ * @param {ChatCompletionCreateParamsNonStreaming["model"]} model - The name of the model to check.
+ * @returns {boolean} - Returns true if the model supports JSON object mode, false otherwise.
+ */
+const isJsonObjectSupportedModel = (model) => {
+    //Model Qian supported by JsonObject, reference https://platform.openai.com/docs/guides/text-generation/json-mode
+    const supportedJsonObjectModelsPrefix = [
+        "gpt-4-turbo",
+        "gpt-3.5-turbo",
+        "gpt-4o",
+    ];
+    return supportedJsonObjectModelsPrefix.some((item) => model.includes(item));
+};
 function getAIResponse(prompt) {
-    var _a, _b;
+    var _a;
     return __awaiter(this, void 0, void 0, function* () {
-        const queryConfig = {
-            model: OPENAI_API_MODEL,
-            temperature: 0.2,
-            max_tokens: 700,
-            top_p: 1,
-            frequency_penalty: 0,
-            presence_penalty: 0,
-        };
+        const queryConfig = Object.assign({ model: OPENAI_API_MODEL, temperature: 0.2, max_tokens: 700, top_p: 1, frequency_penalty: 0, presence_penalty: 0 }, (isJsonObjectSupportedModel(OPENAI_API_MODEL)
+            ? { response_format: { type: "json_object" } }
+            : {}));
         try {
-            const response = yield openai.chat.completions.create(Object.assign(Object.assign(Object.assign({}, queryConfig), (OPENAI_API_MODEL === "gpt-4-1106-preview"
-                ? { response_format: { type: "json_object" } }
-                : {})), { messages: [
+            const response = yield openai.chat.completions.create(Object.assign(Object.assign({}, queryConfig), { messages: [
                     {
                         role: "system",
                         content: prompt,
                     },
                 ] }));
-            const res = ((_b = (_a = response.choices[0].message) === null || _a === void 0 ? void 0 : _a.content) === null || _b === void 0 ? void 0 : _b.trim()) || "{}";
-            return JSON.parse(res).reviews;
+            const rawContent = ((_a = response.choices[0].message) === null || _a === void 0 ? void 0 : _a.content) || "";
+            const startIndex = rawContent.indexOf("{");
+            const endIndex = rawContent.lastIndexOf("}") + 1;
+            if (startIndex === -1 || endIndex === -1) {
+                throw new Error("No valid JSON found in response content.");
+            }
+            const jsonString = rawContent.substring(startIndex, endIndex).trim();
+            return JSON.parse(jsonString).reviews;
         }
         catch (error) {
             console.error("Error:", error);
