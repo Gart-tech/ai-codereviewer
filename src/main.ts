bf9b561253bc47107c044559f7cbcf865ca44714
shkrobik2017 <shkrobik2017@gmail.com>
2024-07-23 15:33:53 +0300

    update-prompt

--- src/main.ts
+++ src/main.ts
@@ -80,6 +80,7 @@ async function analyzeCode(
 
 function createPrompt(file: File, chunk: Chunk, prDetails: PRDetails): string {
   return `Your task is to review pull requests. Instructions:
+- Provide the response in format, like {"reviews": [{"lineNumber":  <line_number>, "reviewComment": "<review comment>"}]}
 - Do not give positive comments or compliments.
 - Provide comments and suggestions ONLY if there is something to improve, otherwise "reviews" should be an empty array.
 - Possible improvements may ONLY include criticisms regarding security or architecture.
